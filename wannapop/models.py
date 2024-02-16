524b157383bcdbfa5d8a15b62696c3fa507a105f
MireiaCanton <camami@fp.insjoaquimmir.cat>
2024-02-16 20:05:41 +0100

    3.2.2-basicauth

--- wannapop/models.py
+++ wannapop/models.py
@@ -13,7 +13,7 @@ class User(UserMixin, db.Model, SerializableMixin, BaseMixin):
     name = db.Column(db.String, unique=True, nullable=False)
     email = db.Column(db.String, unique=True, nullable=False)
     role = db.Column(db.String, nullable=False)
-    __password = db.Column("password", db.String, nullable=False)
+    password = db.Column("password", db.String, nullable=False)
     verified = db.Column(db.Integer, nullable=False)
     email_token = db.Column(db.String, nullable=True, server_default=None)
     created = db.Column(db.DateTime, server_default=func.now())
@@ -22,74 +22,34 @@ class User(UserMixin, db.Model, SerializableMixin, BaseMixin):
     token = db.Column(db.String, unique=True, nullable=True)
     token_expiration = db.Column(db.DateTime, nullable=True)
 
+    # Class variable from SerializableMixin
+    exclude_attr = ['password']
+
     def get_id(self):
         return self.email
-    
+
     def get_token(self, expires_in=3600):
         now = datetime.now(timezone.utc)
-        if self.token and self.token_expiration.replace(tzinfo=timezone.utc) > now + timedelta(seconds=60):
+        if self.token and self.token_expiration.replace(
+                tzinfo=timezone.utc) > now + timedelta(seconds=60):
             return self.token
         self.token = secrets.token_hex(16)
         self.token_expiration = now + timedelta(seconds=expires_in)
-        db.session.add(self)
-        db.session.commit()
+        self.save()
         return self.token
 
     def revoke_token(self):
-        self.token_expiration = datetime.now(timezone.utc) - timedelta(seconds=1)
-        db.session.add(self)
-        db.session.commit()
-
+        self.token_expiration = datetime.now(timezone.utc) - timedelta(
+            seconds=1)
+        self.save()
+        
     @staticmethod
     def check_token(token):
         user = User.get_filtered_by(token=token)
-        if user is None or user.token_expiration.replace(tzinfo=timezone.utc) < datetime.now(timezone.utc):
+        if user is None or user.token_expiration.replace(
+                tzinfo=timezone.utc) < datetime.now(timezone.utc):
             return None
         return user
-    
-    @hybrid_property
-    def password(self):
-        return ""
-    
-    @password.setter
-    def password(self, plain_text_password):
-        self.__password = generate_password_hash(plain_text_password, method="scrypt")
-
-    def check_password(self, some_password):
-        return check_password_hash(self.__password, some_password)
-
-    def is_admin(self):
-        return self.role == "admin"
-
-    def is_moderator(self):
-        return self.role == "moderator"
-    
-    def is_admin_or_moderator(self):
-        return self.is_admin() or self.is_moderator()
-    
-    def is_wanner(self):
-        return self.role == "wanner"
-
-    def is_action_allowed_to_product(self, action, product=None, banned=None):
-        from .helper_role import _permissions, Action
-
-        current_permissions = _permissions[self.role]
-        if not current_permissions:
-            return False
-        
-        if not action in current_permissions:
-            return False
-        
-        if action == Action.products_update and self.is_wanner():
-            return product and self.id == product.seller_id
-        
-        if action == Action.products_delete and self.is_wanner():
-            return product and self.id == product.seller_id
-        
-        if action == Action.products_read and self.is_wanner():
-            return product and (not banned or self.id == product.seller_id)
-        
-        return True
 
 class Product(db.Model, SerializableMixin, BaseMixin):
     __tablename__ = "products"
